b9d6688471b075b409cdb010705b3b39993bfb04
Oncle Picsou <flavargues@users.noreply.github.com>
2021-12-06 13:28:51 +0100

    advancement

--- code/TestNetwork.py
+++ code/TestNetwork.py
@@ -1,65 +1,51 @@
 from docker import *
+from docker.errors import *
+import time
+import re
 
 class TestNetwork():
-#init
 	def __init__(self):
 		self.containerList = []
-		self.dockerDaemon = DockerClient
 
 #main
-	def connect(self, dockerDaemon):
+	def connect(self, dockerDaemon = 'unix:///var/run/docker.sock'):
 		try:
 			newDaemon = DockerClient(base_url=dockerDaemon)
 			if newDaemon.ping():
 				print("Connected to docker daemon.")
+				self.dockerDaemon = newDaemon
 			else:
 				raise Exception("Daemon doesn't ping back.")
-			self.dockerDaemon = newDaemon
-
-		except Exception as error:
-			print("An exception occured !", error)
+		except Exception:
 			print("You can use docker-in-docker with docker-compose up -d and enter the IP address:2376 of the container.")
-
-	#def serverHealthCheck(self):
-	#	try:
-	#		if self.dockerDaemon.ping():
-	#			print("Daemon healthy.")
-	#	except Exception as error:
-	#		print("Daemon error ! Detaching. Please connect().", error)
-	#		self.dockerDaemon = None
-	
-	def start(self):
-		for it in reversed(self.containerList):
-			it.start()
-
+			raise
 
 	def destroy(self):
 		for it in self.containerList:
-			it.remove(force=True)
+			try:
+				it.remove(force=True)
+				self.containerList.pop(0)
+			except Exception as error:
+				pass
 
 	def build(self, topology: str, NContainers: int):
 	#arguments check
 		topology = topology.lower()
-		valid = {'star', 'chain', 'tree', 'ring', 'fully connected'}
-		if topology not in valid:
-			print("Topology unknown. Available values are : ['star', 'chain', 'tree', 'ring', 'fully connected'] (Non-case sensitive)")
-			return
-		if type(NContainers) != int or NContainers < 2:
-			print("Incorrect number of containers. Must be <= 2.")
-			return
-
-		#TODO check if containerlist empty
-
+		if topology not in ['star', 'tree', 'ring', 'fully connected'] or NContainers < 0 or NContainers > 10:
+			raise Exception("Bad arguments", topology, NContainers)
+	#empty containerList
+		if len(self.containerList) > 0:
+			self.destroy()
+		
+	#building topology
 		if   topology == 'star':
 			try:
-			#docker pull openvswitch/ovs:2.12.0_debian
-			
 			#create hosts at 0...NContainers - 1
-				for i in range(1, NContainers):
-					newHost = self.dockerDaemon.containers.create("alpine", command="/bin/sh", tty=True, detach=True,
+				for i in range(NContainers):
+					newContainer = self.dockerDaemon.containers.create("alpine", command="/bin/sh", tty=True, detach=True,
 					name="host{}".format(i)
 					)
-					self.containerList.append(newHost)
+					self.containerList.append(newContainer)
 			#create switch at 0
 				linkDict = dict()
 				for i in range(1, NContainers):
@@ -75,29 +61,82 @@ class TestNetwork():
 				self.destroy()
 
 		elif topology == 'chain':
-			pass
+			for i in range(NContainers):
+				try:
+					containerName = "host{}".format(i)
+					if i == 0:
+						newContainer = self.dockerDaemon.containers.create("alpine", "/bin/sh", detach=True, name=containerName,
+						stdin_open = True)
+						newContainer.start()
+					else:
+						link = dict()
+						newContainer = self.dockerDaemon.containers.create("alpine", "/bin/sh", detach=True, name=containerName,
+						links = dict([(previousName, previousName)]), stdin_open = True)
+						newContainer.start()
+					self.containerList.append(newContainer)
+					previousName = containerName
+					
+				except Exception as error:
+					raise
+			
 		elif topology == 'tree':
 			pass
 		elif topology == 'ring':
 			pass
 		elif topology == 'fully connected':
-			pass
+			for i in range(NContainers):
+				try:
+					newContainer = self.dockerDaemon.containers.create("alpineping", command="/bin/sh",
+						tty=True, detach=True, name="host{}".format(i))
+					self.containerList.append(newContainer)
+					newContainer.start()
+				except Exception as error:
+					self.destroy()
+					raise
 
-		#			LinkingList[i][i + 1] = 1
-		#		for i in range(NContainers - 1):
-		#		LinkingList[0][range(NContainers)] = 1
-		#		LinkingList[range(NContainers)][range(NContainers)] = 1
-		#for i in range(NContainers):
-		#try:
-		#	for i in range(NContainers):
-		#		n = self.dockerDaemon.containers.create('alpine')
-		#		self.containerList.append(n)
-		#except docker.errors.ImageNotFound as error:
-		#	self.dockerDaemon.images.pull
-		#LinkingList = [[0 for col in range(NContainers)] for row in range(NContainers)]
+		time.sleep(3)
+		self.hostsString=self._generateHostsFileString(self._getIPAddr())
+		for it in self.containerList:
+			it.exec_run("ash -c \"echo -e "+"\'" + self.hostsString + "\'" +" >> /temp.txt\"")
+			it.exec_run("ash -c \"cat /temp.txt" + " >> /etc/hosts\"")
+
+	def _getIPAddr(self):
+		ipList = dict()
+		for it in self.containerList:
+			it.reload()
+			ipList[it.attrs['Name']] = it.attrs['NetworkSettings']['Networks']['bridge']['IPAddress']
+		return ipList
+
+	def _generateHostsFileString(self, hostsIP):
+		append = ""
+		for it in self.containerList:
+			name = it.attrs['Name']
+			ip = hostsIP[name]
+			append += ip + " " + name[1:] + "{}".format("\n")
+		return append
+
+	def ping(self, sender:str, receiver:str, duration:int = 5):
+		feedback = self.dockerDaemon.containers.get(sender).exec_run("ping -Aq -c 1000 " + receiver, tty=True)
+		
+		count = feedback.count
+		exit_code = feedback.exit_code
+		index = feedback.index
+		
+		output = str(feedback.output)
+		pt = re.search("(\d{1,}) pa", output)
+		pr = re.search("(\d{1,}) r", output)
+		RTT = re.search("(\d{1,})ms", output)
+
+		temp = re.search("mdev(.{1,}),", output)
+		min = re.findall("\d{1,}.\d{1,}", temp)[0]
+		avg = re.findall("\d{1,}.\d{1,}", temp)[1]
+		max = re.findall("\d{1,}.\d{1,}", temp)[2]
+		mdev = re.findall("\d{1,}.\d{1,}", temp)[3]
+
+		ipg = re.search("ewma (\d{1,}.\d{1,})", output)
+		ewma = re.search("\/(\d{1,}.\d{1,}) ms\\r", output)
+
+		return count, exit_code, index, pt, pr, RTT, temp, min, avg, max, mdev, ipg, ewma
 
 
-
-
-	
-#helper functions
+d run --label "com.docker-tc.limit=1mbps" --label "com.docker-tc.delay=100ms" --label "com.docker-tc.loss=50%" --label "com.docker-tc.duplicate=50%" -it abbey22/traffic-control
