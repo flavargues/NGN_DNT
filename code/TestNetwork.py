c96d0cccc9608a01681adf7dd3786ceb3329b33b
Oncle Picsou <flavargues@users.noreply.github.com>
2021-12-29 15:37:58 +0100

    stash

--- code/TestNetwork.py
+++ code/TestNetwork.py
@@ -1,3 +1,4 @@
+#docker run --label "com.docker-tc.limit=1mbps" --label "com.docker-tc.delay=100ms" --label "com.docker-tc.loss=50%" --label "com.docker-tc.duplicate=50%" -it abbey22/traffic-control
 from docker import *
 from docker.errors import *
 import time
@@ -6,6 +7,7 @@ import re
 class TestNetwork():
 	def __init__(self):
 		self.containerList = []
+		self.networkList = []
 
 #main
 	def connect(self, dockerDaemon = 'unix:///var/run/docker.sock'):
@@ -17,103 +19,117 @@ class TestNetwork():
 			else:
 				raise Exception("Daemon doesn't ping back.")
 		except Exception:
+			print(f"NOT connected to docker daemon at {dockerDaemon}.")
 			print("You can use docker-in-docker with docker-compose up -d and enter the IP address:2376 of the container.")
-			raise
 
 	def destroy(self):
 		for it in self.containerList:
 			try:
 				it.remove(force=True)
-				self.containerList.pop(0)
 			except Exception as error:
 				pass
+		self.dockerDaemon.networks.prune()
+		self.containerList = []
+		self.networkList = []
+		#if len(self.containerList):
+		#	for i in range(10):
+		#		détruire container host{i}
 
-	def build(self, topology: str, NContainers: int):
-	#arguments check
+	def build(self, topology: str, NumberOfContainers: int):
 		topology = topology.lower()
-		if topology not in ['star', 'tree', 'ring', 'fully connected'] or NContainers < 0 or NContainers > 10:
-			raise Exception("Bad arguments", topology, NContainers)
-	#empty containerList
+		if topology not in ['star', 'fully connected'] or NumberOfContainers < 0 or NumberOfContainers > 10:
+			raise Exception("Bad arguments.", topology, NumberOfContainers)
 		if len(self.containerList) > 0:
 			self.destroy()
 		
-	#building topology
-		if   topology == 'star':
-			try:
-			#create hosts at 0...NContainers - 1
-				for i in range(NContainers):
-					newContainer = self.dockerDaemon.containers.create("alpine", command="/bin/sh", tty=True, detach=True,
-					name="host{}".format(i)
-					)
-					self.containerList.append(newContainer)
-			#create switch at 0
-				linkDict = dict()
-				for i in range(1, NContainers):
-					linkDict["host{}".format(i)] = "host{}".format(i)
-					
-				switch = self.dockerDaemon.containers.create("globocom/openvswitch", detach=True,
-				name="switch", links=linkDict, tty=True, cap_add = ['NET_ADMIN'], stdin_open = True
-				)
-				self.containerList.insert(0, switch)
 
-			except Exception as error:
-				print("Error during network building ! Destroying containers.", error)
-				self.destroy()
-
-		elif topology == 'chain':
-			for i in range(NContainers):
-				try:
-					containerName = "host{}".format(i)
-					if i == 0:
-						newContainer = self.dockerDaemon.containers.create("alpine", "/bin/sh", detach=True, name=containerName,
-						stdin_open = True)
-						newContainer.start()
-					else:
-						link = dict()
-						newContainer = self.dockerDaemon.containers.create("alpine", "/bin/sh", detach=True, name=containerName,
-						links = dict([(previousName, previousName)]), stdin_open = True)
-						newContainer.start()
-					self.containerList.append(newContainer)
-					previousName = containerName
-					
-				except Exception as error:
-					raise
-			
-		elif topology == 'tree':
-			pass
-		elif topology == 'ring':
-			pass
-		elif topology == 'fully connected':
-			for i in range(NContainers):
+		if topology == "fully connected":
+			for i in range(NumberOfContainers):
 				try:
 					newContainer = self.dockerDaemon.containers.create("alpineping", command="/bin/sh",
-						tty=True, detach=True, name="host{}".format(i))
+						tty=True, detach=True, cap_add=["NET_ADMIN"], name=f"host{i}")
+						
 					self.containerList.append(newContainer)
 					newContainer.start()
 				except Exception as error:
 					self.destroy()
 					raise
 
-		time.sleep(3)
-		self.hostsString=self._generateHostsFileString(self._getIPAddr())
-		for it in self.containerList:
-			it.exec_run("ash -c \"echo -e "+"\'" + self.hostsString + "\'" +" >> /temp.txt\"")
-			it.exec_run("ash -c \"cat /temp.txt" + " >> /etc/hosts\"")
+		elif topology == "star":
+			for i in range(NumberOfContainers):
+				try:
+					if i == 0:
+						for j in range(1, NumberOfContainers):
+							self.networkList.append(self.dockerDaemon.networks.create(f"b0-{j}"))
 
-	def _getIPAddr(self):
-		ipList = dict()
+						newContainer = self.dockerDaemon.containers.create("alpineping", command="/bin/sh",
+							tty=True, detach=True, cap_add=["NET_ADMIN"] , name="host0")
+						self.containerList.append(newContainer)
+
+						for it in self.networkList:
+							it.connect("host0")
+						
+					else:
+						newContainer = self.dockerDaemon.containers.create("alpineping", command="/bin/sh",
+							tty=True, detach=True, cap_add=["NET_ADMIN"], name=f"host{i}", network=f"b0-{i}")
+						self.containerList.append(newContainer)
+						
+					newContainer.start()
+					time.sleep(1)
+
+				except Exception as error:
+					self.destroy()
+					raise
+			time.sleep(2)
+
+			gateways = dict()
+			center = self.containerList[0]
+			center.reload()
+			for network in self.networkList:
+				network.reload()
+				gateways[network.name] = center.attrs['NetworkSettings']['Networks'][network.name]['IPAddress']
+			for edge in self.containerList[1:]:
+				edge.reload()
+				myNetwork = list(edge.attrs['NetworkSettings']['Networks'])[0]
+				myGateway = gateways[myNetwork]
+				output = edge.exec_run(f"ip route del default;	ip route add default via {myGateway}", tty=True)
+
+			"""gateways = dict()
+			for it in self.containerList:
+				it.reload()
+			for it in self.containerList:
+				if it.name == "host0":
+					networks = list(it.attrs['NetworkSettings']['Networks'].keys())
+					for key in networks:
+						gateways[key] = it.attrs['NetworkSettings']['Networks'][str(key)]['IPAddress']
+				elif it.name != "host0":
+					myGateway = gateways.index(str(list(	it.attrs['NetworkSettings']['Networks'].keys() )[0]   ))
+					myGateway = gateways[str(list(it.attrs['NetworkSettings']['Networks'].keys())[0])]
+					it.exec_run(f"ip route del default; ip route add default via {myGateway}")"""
+		#add host names
+		hostsFile = ""
 		for it in self.containerList:
 			it.reload()
-			ipList[it.attrs['Name']] = it.attrs['NetworkSettings']['Networks']['bridge']['IPAddress']
-		return ipList
-
-	def _generateHostsFileString(self, hostsIP):
-		append = ""
+			itOneNetwork = it.attrs['NetworkSettings']['Networks'].keys()[0]
+			itIP = it.attrs['NetworkSettings']['Networks'][itOneNetwork]["IPAddress"]
+			hostsFile += itIP + " " + it.name[1:] + "{}".format("\n")
 		for it in self.containerList:
-			name = it.attrs['Name']
-			ip = hostsIP[name]
-			append += ip + " " + name[1:] + "{}".format("\n")
-		return append
+			it.exec_run("echo " + hostsFile + " >> /etc/hosts")
+		
+		
+	#def getIPAddr(self):
+	#	ipList = dict()
+	#	for it in self.containerList:
+	#		it.reload()
+	#		ipList[it.attrs['Name']] = it.attrs['NetworkSettings']['Networks']['bridge']['IPAddress']
+	#	return ipList
+	#def generateHostsFileString(self, hostsIP):
+	#	append = ""
+	#	for it in self.containerList:
+	#		name = it.attrs['Name']
+	#		ip = hostsIP[name]
+	#		append += ip + " " + name[1:] + "{}".format("\n")
+	#	return append
 
 	def ping(self, sender:str, receiver:str, duration:int = 5):
 		feedback = self.dockerDaemon.containers.get(sender).exec_run("ping -Aq -c 1000 " + receiver, tty=True)
@@ -125,7 +141,7 @@ class TestNetwork():
 		output = str(feedback.output)
 		pt = re.search("(\d{1,}) pa", output)
 		pr = re.search("(\d{1,}) r", output)
-		RTT = re.search("(\d{1,})ms", output)
+		rtt = re.search("(\d{1,})ms", output)
 
 		temp = re.search("mdev(.{1,}),", output)
 		min = re.findall("\d{1,}.\d{1,}", temp)[0]
@@ -136,7 +152,18 @@ class TestNetwork():
 		ipg = re.search("ewma (\d{1,}.\d{1,})", output)
 		ewma = re.search("\/(\d{1,}.\d{1,}) ms\\r", output)
 
-		return count, exit_code, index, pt, pr, RTT, temp, min, avg, max, mdev, ipg, ewma
+		return count, exit_code, index, pt, pr, rtt , temp, min, avg, max, mdev, ipg, ewma
 
 
-d run --label "com.docker-tc.limit=1mbps" --label "com.docker-tc.delay=100ms" --label "com.docker-tc.loss=50%" --label "com.docker-tc.duplicate=50%" -it abbey22/traffic-control
+"""
+for twamp
+	add file for the github
+	compile them
+for ping
+	apk get install iputils
+for bandwidth
+	apt get install iftops
+OWAMP ?
+	???
+
+"""
